b62e1e1185a657e2fa566095286bab29b175246b
dev-suman <ksuman20601028@gmail.com>
2026-02-09 23:28:13 +0545

    fix:socket connetion for users:

--- client/src/routes/_dashboard/dashboard/questions.tsx
+++ client/src/routes/_dashboard/dashboard/questions.tsx
@@ -1,6 +1,23 @@
-import { QuestionsAnswers } from '@/components/pages/dashboard/questionsAnswers'
-import { createFileRoute } from '@tanstack/react-router'
+import { QuestionsAnswers } from "@/components/pages/dashboard/questionsAnswers";
+import { useSocket } from "@/context/socketContext";
+import { createFileRoute } from "@tanstack/react-router";
+import { useEffect } from "react";
 
-export const Route = createFileRoute('/_dashboard/dashboard/questions')({
-  component: QuestionsAnswers,
-})
+export const Route = createFileRoute("/_dashboard/dashboard/questions")({
+  component: QuestionPage,
+});
+
+function QuestionPage() {
+  const socket = useSocket();
+
+  useEffect(() => {
+    if (!socket) return;
+
+    socket.emit("join:questions-feed");
+
+    return () => {
+      socket.emit("leave:questions-feed");
+    };
+  }, [socket]);
+  return <QuestionsAnswers />;
+}
--- server/src/socket/question.socket.ts
+++ server/src/socket/question.socket.ts
@@ -1,6 +1,13 @@
 import { Server, Socket } from "socket.io";
 
 export const registerQuestionSocket = (io: Server, socket: Socket) => {
-  socket.join("questions-feed");
-    console.log(`${socket.id} auto-joined questions-feed`);
+   socket.on("join:questions-feed", () => {
+      socket.join("questions-feed");
+      console.log(`${socket.id} joined questions-feed`);
+   });
+
+   socket.on("leave:questions-feed", () => {
+      socket.leave("questions-feed");
+      console.log(`${socket.id} left questions-feed`);
+   });
 };
