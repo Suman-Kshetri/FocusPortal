e03abc432a8e25895ab9f263fc8f4c1d455a8c9a
dev-suman <ksuman20601028@gmail.com>
2026-02-14 23:38:52 +0545

    feat&fix:created routes and controller for the files operation and fixed routes

--- server/src/routes/files.route.ts
+++ server/src/routes/files.route.ts
@@ -1,3 +1,4 @@
+// routes/files.route.ts
 import express from "express";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
 import { docsUpload } from "../middlewares/docsUpload.middleware.js";
@@ -15,16 +16,23 @@ import {
 } from "../controllers/files.controller.js";
 
 const filesRoute = express.Router();
+
 filesRoute.use(verifyJwt);
+
+// File upload and creation
 filesRoute.post("/upload", docsUpload.array("files", 10), uploadFile);
 filesRoute.post("/create", createFile);
-filesRoute.get("/folder/:folderId", listFiles);
-filesRoute.get("/:fileId", getFile);
-filesRoute.get("/:fileId/content", readFileContent);
-filesRoute.get("/:fileId/download", downloadFile);
-filesRoute.put("/:fileId/edit", editFile);
-filesRoute.put("/:fileId/move", moveFile);
-filesRoute.patch("/:fileId/rename", renameFile);
-filesRoute.delete("/:fileId/delete", deleteFile);
+
+// File listing
+filesRoute.get("/folder/:folderId/files", listFiles);
+
+// File operations
+filesRoute.get("/:id", getFile);
+filesRoute.get("/:id/content", readFileContent);
+filesRoute.get("/:id/download", downloadFile);
+filesRoute.patch("/:id/edit", editFile);
+filesRoute.patch("/:id/move", moveFile);
+filesRoute.patch("/:id/rename", renameFile);
+filesRoute.delete("/:id/delete", deleteFile);
 
 export default filesRoute;
